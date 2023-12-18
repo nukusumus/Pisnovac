214f3e3124c90ed4fff90682c7537c52fc81691e
nukusumus <marek.g.sterba@gmail.com>
2023-12-18 21:03:27 +0100

    pisnovac skript

--- Spustit Pisnovac.bat
+++ Spustit Pisnovac.bat
@@ -2,20 +2,10 @@
 
 cd Src
 
-wget https://www.stud.fit.vutbr.cz/~xsterb16/Downloads/files/new_version.txt
+curl https://www.stud.fit.vutbr.cz/~xsterb16/Downloads/files/pisnovac.txt > pisnovac.py
 
-fc version.txt new_version.txt > nul
-if errorlevel 1 goto stahnout
-
-goto run
-
-:stahnout
-wget https://www.stud.fit.vutbr.cz/~xsterb16/Downloads/files/pisnovac.txt
-mv pisnovac.txt pisnovac.py
-
-:run
-cls
-rm new_version.txt
+echo:
+echo Pisnovac: Nezavirejte toto okno, zavre se automaticky
 cd ..
 call "%cd%\venv\Scripts\activate"
 
